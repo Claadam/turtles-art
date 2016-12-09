bc98ac73d715fa8ff1294e6b4b35e92ea1cdd1c7
WalnutsAreCool <claadam@nuevaschool.org>
2016-12-09 13:49:02 -0800

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,13 @@
+function nSpiralRing(n) {
+  for (j=0; j<360/n; j++){
+    goForward(180/n)
+    nDegreesLeft((720/n)+1)
+    goForward(360/n)
+    nDegreesRight((360/n))
+  }  
+}
+
+for (q=0; q<12; q++){
+  nSpiralRing(4)
+  nSpiralRing(3)
+}
