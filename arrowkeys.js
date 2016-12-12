85f6d2d473a92344b33da1f85ff361e70b5b95ac
WalnutsAreCool <claadam@nuevaschool.org>
2016-12-12 08:52:49 -0800

    Classadoo autocommit

--- arrowkeys.js
+++ arrowkeys.js
@@ -1,21 +1,23 @@
-$("#polygon").keydown(function(e) {	
-  if (e.keyCode == "39") {
-    nDegreesRight(10)
-  }
-})
+//$("#polygon").keydown(function(e) {	
+//  if (e.keyCode == "39") {
+//    nDegreesRight(10)
+//  }
+//})
+//
+//$("#polygon").keydown(function(e) {	
+//  if (e.keyCode == "38") {
+//   goForward(10)
+//  }
+//})
+//
+//$("#polygon").keydown(function(e) {	
+//  if (e.keyCode == "37") {
+//    nDegreesRight(10)
+//  }
+//})
 
-$("#polygon").keydown(function(e) {	
-  if (e.keyCode == "38") {
-   goForward(10)
-  }
-})
+gameInput.keydown(function(keydownEvent) {
 
-$("#polygon").keydown(function(e) {	
-  if (e.keyCode == "37") {
-    nDegreesRight(10)
-  }
-})
-
-if(.keycode == "39") {
+if(keydownEvent.keycode == "39") {
   nDegreesRight(10)
 }
