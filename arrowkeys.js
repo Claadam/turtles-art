c8811b06564a2e757ae2aa443ab008e8f15193f2
WalnutsAreCool <claadam@nuevaschool.org>
2016-12-12 08:51:03 -0800

    Classadoo autocommit

--- arrowkeys.js
+++ arrowkeys.js
@@ -1,17 +1,21 @@
-.keydown(function(e) {	
+$("#polygon").keydown(function(e) {	
   if (e.keyCode == "39") {
     nDegreesRight(10)
   }
 })
 
-.keydown(function(e) {	
+$("#polygon").keydown(function(e) {	
   if (e.keyCode == "38") {
    goForward(10)
   }
 })
 
-.keydown(function(e) {	
+$("#polygon").keydown(function(e) {	
   if (e.keyCode == "37") {
     nDegreesRight(10)
   }
 })
+
+if(.keycode == "39") {
+  nDegreesRight(10)
+}
